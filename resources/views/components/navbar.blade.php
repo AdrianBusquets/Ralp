e588fbddd05d329ec7a0bcb46bd7608cd6bee29e
Adrian <adrian.busquets@hotmail.com>
2023-08-29 22:12:26 +0200

    LOGIN Y REGISTER

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -35,16 +35,31 @@
             </li>
         </ul>
         <ul>
+        @guest
+            @if(Route::has('login'))
             <li>
-            <a href="#signup">
-                Sign-up
-            </a>
-            </li>
+                <a href="{{ route('login') }}">
+                    Entrar
+                </a>
+                </li>
+            @endif
+            @if (Route::has('register'))
             <li>
-            <a href="#login">
-                Login
-            </a>
-            </li>
+                <a href="{{ route('register') }}">
+                    Registrar
+                </a>
+                </li>
+            @endif
+            @else
+            <li>
+                <form id="logoutForm" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                </form>
+                <a id="logoutBtn" href="#">
+                    Salir
+                </a>
+                </li>
+        @endguest
         </ul>
         </div>
     </nav>
